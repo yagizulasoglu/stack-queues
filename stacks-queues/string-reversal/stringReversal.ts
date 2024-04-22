f09a23828ef85c46aa072a36f0b4d9b8b248f82d
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-04-22 15:55:02 -0700

    string reversal complete

--- stacks-queues/string-reversal/stringReversal.ts
+++ stacks-queues/string-reversal/stringReversal.ts
@@ -1,9 +1,18 @@
 import { StackStr } from "../common/stack";
+import { IndexError } from "../common/ll";
 
 /** Return reverse of string. */
 
 function stringReversal(str: string): string {
-  return "x";
+  let newStr= "";
+
+  if (str === "") throw new IndexError;
+
+  for (let i = str.length-1; i >=0; i--) {
+    newStr! += str[i];
+  }
+
+  return newStr!;
 }
 
 export { stringReversal };
